8ce5be93e96bfb00a2c1eac19b2a5e9537b35b98
Resper <respernyivuru@gmail.com>
2023-08-25 17:20:43 +0300

    Testcase written in python using vs code, selenium

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ from selenium import webdriver
 driver = webdriver.Chrome()  
 driver.maximize_window()  
 driver.get("https://www.google.com/")  
-driver.find_element_by_id("input").send_keys("javatpoint")
-#driver.find_element()  
+driver.find_element(by='id', value='input').send_keys("javatpoint")
+driver.close()  
